ea9c6a73870bad31546612d3ea0e9a34d707f0a4
RAVIROCK916 <prt15@iitbbs.ac.in>
2024-05-13 10:39:06 +0530

    feat: initiate clerk provider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "selector",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +8,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        dark: {
+          DEFAULT: "#222831",
+          1: "#31363F",
+        },
+        light: {
+          DEFAULT: "#76ABAE",
+          1: "#EEEEEE",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
